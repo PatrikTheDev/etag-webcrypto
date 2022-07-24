763e9deaaf7a8a486de266b9dedd73e80636e570
Patrik Svoboda <svob.pata@gmail.com>
2022-07-24 18:13:11 +0200

    finished support

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*!
- * etag
- * Copyright(c) 2014-2016 Douglas Christopher Wilson
+ * etag-webcrypto
+ * Copyright(c) 2014-2016 Douglas Christopher Wilson, 2022 Patrik Svoboda
  * MIT Licensed
  */
 
@@ -21,14 +21,6 @@ module.exports = etag
 const { Crypto } = require('@peculiar/webcrypto')
 
 const crypto = new Crypto()
-var Stats = require('fs').Stats
-
-/**
- * Module variables.
- * @private
- */
-
-var toString = Object.prototype.toString
 
 /**
  * Generate an entity tag.
@@ -45,14 +37,14 @@ function entitytag(entity) {
   }
 
   // compute hash of entity
-  var hash = crypto
+  const hash = crypto
     .createHash('sha1')
     .update(entity, 'utf8')
     .digest('base64')
     .substring(0, 27)
 
   // compute length of entity
-  var len =
+  const len =
     typeof entity === 'string'
       ? Buffer.byteLength(entity, 'utf8')
       : entity.length
@@ -63,7 +55,7 @@ function entitytag(entity) {
 /**
  * Create a simple ETag.
  *
- * @param {string|Buffer|Stats} entity
+ * @param {string|Buffer} entity
  * @param {object} [options]
  * @param {boolean} [options.weak]
  * @return {String}
@@ -71,66 +63,18 @@ function entitytag(entity) {
  */
 
 function etag(entity, options) {
-  if (entity == null) {
+  if (!entity) {
     throw new TypeError('argument entity is required')
   }
-
-  // support fs.Stats object
-  var isStats = isstats(entity)
-  var weak =
-    options && typeof options.weak === 'boolean' ? options.weak : isStats
+  const weak = options && typeof options.weak === 'boolean' && options.weak
 
   // validate argument
-  if (!isStats && typeof entity !== 'string' && !Buffer.isBuffer(entity)) {
+  if (typeof entity !== 'string' && !Buffer.isBuffer(entity)) {
     throw new TypeError('argument entity must be string, Buffer, or fs.Stats')
   }
 
   // generate entity tag
-  var tag = isStats ? stattag(entity) : entitytag(entity)
+  const tag = entitytag(entity)
 
   return weak ? 'W/' + tag : tag
-}
-
-/**
- * Determine if object is a Stats object.
- *
- * @param {object} obj
- * @return {boolean}
- * @api private
- */
-
-function isstats(obj) {
-  // genuine fs.Stats
-  if (typeof Stats === 'function' && obj instanceof Stats) {
-    return true
-  }
-
-  // quack quack
-  return (
-    obj &&
-    typeof obj === 'object' &&
-    'ctime' in obj &&
-    toString.call(obj.ctime) === '[object Date]' &&
-    'mtime' in obj &&
-    toString.call(obj.mtime) === '[object Date]' &&
-    'ino' in obj &&
-    typeof obj.ino === 'number' &&
-    'size' in obj &&
-    typeof obj.size === 'number'
-  )
-}
-
-/**
- * Generate a tag for a stat.
- *
- * @param {object} stat
- * @return {string}
- * @private
- */
-
-function stattag(stat) {
-  var mtime = stat.mtime.getTime().toString(16)
-  var size = stat.size.toString(16)
-
-  return '"' + size + '-' + mtime + '"'
 }
